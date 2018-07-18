db90987f4c4023165f8103a5891c43b49dc0fa40
Henrik Böttcher <enovateweb@gmail.com>
2018-07-18 15:18:41 +0200

    Use settings struct to define check params

--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/hb-dev/bahn-alerts/checker"
 )
 
-func main() {
-	locationID := 8011956 // Jena Paradies
-	// locationID := 8010205 // Leipzig Hbf
-	time := "06:50"
-	// time := "15:46"
-	trainName := "ICE 1526"
-	// trainName := "IC 2060"
-	daysOfInterest := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+type setting struct {
+	LocationID     int
+	TrainName      string
+	DepartureTime  string
+	DaysOfInterest []string
+	Limit          int
+}
 
-	// limit > 9 -> 500 Response from bahn API
-	_, changedDepartureTimes, err := checker.Check(locationID, daysOfInterest, time, trainName, 9)
-	if err != nil {
-		panic(err)
+type settings []setting
+
+func main() {
+
+	sets := settings{
+		setting{
+			LocationID:     8011956, // Jena Paradies
+			TrainName:      "ICE 1526",
+			DepartureTime:  "06:52",
+			DaysOfInterest: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"},
+			Limit:          4, // limit > 9 -> 500 Response from bahn API
+		},
+		setting{
+			LocationID:     8010205, // Leipzig Hbf
+			TrainName:      "IC 2060",
+			DepartureTime:  "15:46",
+			DaysOfInterest: []string{"Monday", "Tuesday", "Wednesday", "Thursday"},
+			Limit:          4, // limit > 9 -> 500 Response from bahn API
+		},
 	}
 
-	fmt.Println("Changed departure Times:", changedDepartureTimes)
+	for {
+		for _, set := range sets {
+			_, times, err := checker.Check(set.LocationID, set.DaysOfInterest, set.DepartureTime, set.TrainName, set.Limit)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("Changed departure Times for %s: %v\n", set.TrainName, times)
+		}
+		time.Sleep(1 * time.Minute)
+	}
 
 	// define alert parameters
 	// alert changed schedule
